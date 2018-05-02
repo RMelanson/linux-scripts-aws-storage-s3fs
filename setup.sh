86ba8b44eed60830208a336d11e187097465ced5
root <root@ip-172-31-13-110.ca-central-1.compute.internal>
2018-05-02 13:03:22 +0000

    WIP

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 fuseDir=/tmp/s3fs-fuse
-callingDir=pwd
-cd $installDir
+s3fsInstDir=$PWD
 pvtKey=AKIAILM6IJE5QG26I6CQ
 pubKey=kQhOnXfkdSzoAVYoMPjE7lS+Fa7LrQeqkbcUr0b4
+
+#install required unix files directly
+rsync -avhu --progress Source Destination linux/* /
 
 # Download the required Development libraries
 #. ./devToolsInstall.sh
@@ -17,7 +19,6 @@ yum install openssl-devel -y
 # Download the s3fs fuse libs
 if [ ! -d "$fuseDir" ]
 then
-   s3fsInstDir=$PWD
    cd /tmp
    git clone https://github.com/s3fs-fuse/s3fs-fuse.git
    # Build, Configure snd install s3fs
@@ -30,17 +31,12 @@ then
 fi
 
 # add the s3fs access credentials built from IAM with s3 full access rights
-echo $pvtKey:pubKey > /etc/passwd-s3fs
+echo $pvtKey:$pubKey > /etc/passwd-s3fs
 #chown root:root /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
 
-echo y | cp passwd-s3fs /etc
-
 #Set s3fs as an init.d service
 #rm -rf /etc/init.d/s3fsMounts /etc/init.d/s3fs*
-
-#install required unix files directly
-rsync -avhu --progress Source Destination linux/* /
 
 chkconfig s3fs on
 service s3fs start
