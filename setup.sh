9848cac75193e8ee675085d2293dfca36fcfd03f
root <root@ip-172-31-13-167.ca-central-1.compute.internal>
2018-05-02 06:15:31 +0000

    WIP

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-installDir=/var/scripts/apps/S3FS
 fuseDir=/tmp/s3fs-fuse
 callingDir=pwd
 cd $installDir
+pvtKey=AKIAILM6IJE5QG26I6CQ
+pubKey=kQhOnXfkdSzoAVYoMPjE7lS+Fa7LrQeqkbcUr0b4
 
 # Download the required Development libraries
 #. ./devToolsInstall.sh
@@ -28,10 +29,10 @@ then
    cd $s3fsInstDir
 fi
 
-cd $installDir
 # add the s3fs access credentials built from IAM with s3 full access rights
-chown root:root linux/etc/passwd-s3fs
-chmod 600 linux/etc/passwd-s3fs
+echo $pvtKey:pubKey > /etc/passwd-s3fs
+#chown root:root /etc/passwd-s3fs
+chmod 600 /etc/passwd-s3fs
 
 echo y | cp passwd-s3fs /etc
 
