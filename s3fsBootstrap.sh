7436ccc441cc760ffd34e3af06a85cc98f4b23e7
Robin Melanson <remcogold@gmail.com>
2019-03-17 01:27:17 -0400

    Update s3fsBootstrap.sh

--- s3fsBootstrap.sh
+++ s3fsBootstrap.sh
@@ -12,7 +12,9 @@ yum update -y
 yum install git -y
 
 # SETUP ENVIRONMENT AND PARAMETERS
-. ./env/setEnv.sh
+pkg=S3FS
+gitRepo="linux-aws-scripts-utils-s3fs.git"
+installDir="/tmp/scripts/apps/$pkg"
 
 if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone git@github.com:RMelanson/"
