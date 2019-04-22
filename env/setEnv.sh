104dd8f2b9db88fe5b951e67dd82f011699e8429
Robin Melanson <remcogold@gmail.com>
2019-04-22 12:46:25 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # S3FS CONFIGURATION PARAMETERS
-pkg=S3FS
 bootstrap="s3fsBootstrap.sh"
-
 gitRepo="linux-aws-scripts-utils-s3fs.git"
-installDir="/tmp/scripts/apps/$pkg"
+
+#SET UP INSTALLATION DIRECTORY
+pkg=S3FS
+scriptType="apps"
+parentDir="/tmp/scripts/$scriptType/"
+installDir="$parentDir/$pkg"
